e13462bd67498bbee36f8d14c4de7939235ab4fa
Mike Thompson <mike@thomp-mbp.local>
2014-04-14 22:24:04 -0700

    added gold fields and started on the +/- counters for rare mobs

--- d3stats.py
+++ d3stats.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-from datetime import datetime
+
+m datetime import datetime
 import Tkinter, time
 
 ############
@@ -18,6 +18,17 @@ run_time_var = Tkinter.StringVar()
 
 ## Paragon checkbox var
 paragon_lvl_var = Tkinter.IntVar()
+
+## XP entry field var
+xp_before_var = Tkinter.StringVar()
+xp_after_var = Tkinter.StringVar()
+
+## Gold vars
+gold_before_var = Tkinter.StringVar()
+gold_after_var = Tkinter.StringVar()
+
+## yellow mob vars
+yellow_mob_var = Tkinter.IntVar()
 
 ##########
 ## Functions
@@ -41,6 +52,21 @@ def clear_time():
 	stop_time_var.set("")
 	run_time_var.set("")
 
+def find_xp_gain():
+    if xp_before_var.get() != "" and xp_after_var.get() != "":
+        xp_b = (int)(xp_before_var.get())
+        xp_a = (int)(xp_after_var.get())
+        change = abs(xp_a - xp_b)
+    #else:
+        # create pop up error message
+        # tell user to put proper input
+
+def yellow_subtract():
+    yellow_mob_var.set(yellow_mob_var.get()-1)
+    
+def yellow_add():
+    yellow_mob_var.set(yellow_mob_var.get()+1)
+
 ###############
 # Layout management
 ################
@@ -50,8 +76,15 @@ stop_time_label = Tkinter.Label(top,text="Stop Time").grid(row=1,column=2)
 total_time_label = Tkinter.Label(top,text="Run Length").grid(row=2,column=2)
 
 ##XP Labels
-xp_before_label = Tkinter.Label(top,text="XP Before",fg='red').grid(row=4,column=0)
-xp_after_label = Tkinter.Label(top,text="XP After",fg='red').grid(row=5,column=0)
+xp_before_label = Tkinter.Label(top,text="XP Before",fg='red')
+xp_before_label.grid(row=4,column=0)
+xp_after_label = Tkinter.Label(top,text="XP After",fg='red')
+xp_after_label.grid(row=5,column=0)
+
+## MOB Labels
+blue_mob_label = Tkinter.Label(top,text="Blue Mobs",bg='blue',fg='white').grid(row=8,column=0)
+yellow_mob_label = Tkinter.Label(top,text="Yellow Mobs",bg='yellow').grid(row=9,column=0)
+gob_mob_label = Tkinter.Label(top,text="Treasure Gobs",bg='brown').grid(row=10,column=0)
 
 ## Timestamp fields
 start_timestamp = Tkinter.Label(top,textvariable=start_time_var,width=10).grid(row=0,column=1)
@@ -59,13 +92,23 @@ stop_timestamp = Tkinter.Label(top,textvariable=stop_time_var,width=10).grid(row
 total_timestamp = Tkinter.Label(top,textvariable=run_time_var,width=10).grid(row=2,column=1)
 
 ##XP fields
-xp_before_field1 = Tkinter.Entry(top,width=12).grid(row=4,column=1)#,columnspan=2)
+xp_before_field1 = Tkinter.Entry(top,textvariable=xp_before_var,width=12)
+xp_before_field1.grid(row=4,column=1)#,columnspan=2)
 xp_before_field2 = Tkinter.Entry(top,width=12).grid(row=4,column=2)#,columnspan=2)
 xp_level_field1 = Tkinter.Entry(top,width=3).grid(row=4,column=3)
 
-xp_after_field1 = Tkinter.Entry(top,width=12).grid(row=5,column=1)#,columnspan=2)
+xp_after_field1 = Tkinter.Entry(top,textvariable=xp_after_var,width=12)
+xp_after_field1.grid(row=5,column=1)#,columnspan=2)
 xp_after_field2 = Tkinter.Entry(top,width=12).grid(row=5,column=2)#,columnspan=2)
 xp_level_field2 = Tkinter.Entry(top,width=3).grid(row=5,column=3)
+
+## gold fields
+start_gold_label = Tkinter.Label(top,text='Starting Gold',width=10).grid(row=6,column=0)
+end_gold_label = Tkinter.Label(top,text='Ending Gold',width=10).grid(row=7,column=0)
+start_gold_field = Tkinter.Entry(top,textvariable=gold_before_var,width=15).grid(row=6,column=1)
+end_gold_field = Tkinter.Entry(top,textvariable=gold_after_var,width=15).grid(row=7,column=1)
+
+
 
 ## Paragon level check buttons
 paragon_lvl_checkbox = Tkinter.Checkbutton(top,text="Paragon",variable=paragon_lvl_var).grid(row=4,column=4,rowspan=2)
@@ -75,8 +118,18 @@ start_timer_button = Tkinter.Button(top,text="Start",command=update_time).grid(r
 stop_timer_button = Tkinter.Button(top,text="Stop",command=update_time).grid(row=1,column=0)
 clear_timers_button = Tkinter.Button(top,text="Clear",command=clear_time).grid(row=2,column=0)
 
+# XP Button
+#xp_change_button = Tkinter.Button(top,text="Change",command=find_xp_gain).grid(row=6,column=0)
+
+## yellow mobs
+yellow_subtract_button= Tkinter.Button(top,text="-",command=yellow_subtract).grid(row=8,column=1)
+yellow_label = Tkinter.Entry(top,textvariable=yellow_mob_var,width=3).grid(row=8,column=2)
+yellow_add_button= Tkinter.Button(top,text="+",command=yellow_add).grid(row=8,column=3)
+
+
 ####
 # Mainloop
 ###
 top.mainloop()
+
 
