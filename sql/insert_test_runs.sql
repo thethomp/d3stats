2b05210e9d71a6e4365aa9909826c7fea2a562cb
Mike Thompson <mike@thomp-mbp.local>
2014-05-07 21:03:55 -0700

    insert stmt for adding test data to db

---
+++ sql/insert_test_runs.sql
@@ -0,0 +1,1 @@
+INSERT INTO TABLE run (total_time, map_id, xp_gain, legend_drops, goblin_count, gold_gain, y_mobs, b_mobs, p_mobs, difficulty, notes) VALUES ('1:30:00',3,10000,1,1,1000000,2,2,2,3,'this is a test');
